Slayer95/Pokemon-Showdown

commit 529281272a807f06996666360ff82f8cda46c2de

RNG: Shield Dust
Shield Dust does not even permit rolls for secondary effects. This will
correct its interaction with the RNG.

data/abilities.js
@@ -2328,9 +2328,15 @@ exports.BattleAbilities = {
 	"shielddust": {
 		desc: "If the opponent uses a move that has secondary effects that affect this Pokemon in addition to damage, the move's secondary effects will not trigger (For example, Ice Beam loses its 10% freeze chance).",
 		shortDesc: "This Pokemon is not affected by the secondary effect of another Pokemon's attack.",
-		onTrySecondaryHit: function () {
-			this.debug('Shield Dust prevent secondary');
-			return null;
+		onFoeModifyMovePriority: -10,
+		onFoeModifyMove: function (move, pokemon) {
+			if (!move.secondaries) return;
+			if (pokemon.hasAbility(['moldbreaker', 'turboblaze', 'teravolt'])) return;
+			// Remove secondary effects that affect the target.
+			move.secondaries = move.secondaries.filter(function (effect) {
+				if (effect.self) return true;
+				return false;
+			});
 		},
 		id: "shielddust",
 		name: "Shield Dust",

commit 04a9757772e1832675a3ece50f3bc96096498ada

Fix bug introduced in 529281272a807f06996666360ff82f8cda46c2de

data/abilities.js
@@ -2329,9 +2329,10 @@ exports.BattleAbilities = {
 		desc: "If the opponent uses a move that has secondary effects that affect this Pokemon in addition to damage, the move's secondary effects will not trigger (For example, Ice Beam loses its 10% freeze chance).",
 		shortDesc: "This Pokemon is not affected by the secondary effect of another Pokemon's attack.",
 		onFoeModifyMovePriority: -10,
-		onFoeModifyMove: function (move, pokemon) {
+		onFoeModifyMove: function (move, attacker, defender) {
+			if (this.effectData.target !== defender) return;
 			if (!move.secondaries) return;
-			if (pokemon.hasAbility(['moldbreaker', 'turboblaze', 'teravolt'])) return;
+			if (attacker.hasAbility(['moldbreaker', 'turboblaze', 'teravolt'])) return;
 			// Remove secondary effects that affect the target.
 			move.secondaries = move.secondaries.filter(function (effect) {
 				if (effect.self) return true;