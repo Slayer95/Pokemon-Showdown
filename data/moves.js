Slayer95/Pokemon-Showdown

commit 555bae44d10f508449e5e68f747c583dbad15a9d

Include Status moves in lastAttackedBy
This is needed for Mirror Move to work correctly. This also fixes a rare
Focus Punch bug involving 0 damage.

data/moves.js
@@ -4704,7 +4704,7 @@ exports.BattleMovedex = {
 			if (!pokemon.removeVolatile('focuspunch')) {
 				return false;
 			}
-			if (pokemon.lastAttackedBy && pokemon.lastAttackedBy.damage && pokemon.lastAttackedBy.thisTurn) {
+			if (pokemon.lastAttackedBy && pokemon.lastAttackedBy.thisTurn && this.getMove(pokemon.lastAttackedBy.move).category !== 'Status') {
 				this.add('cant', pokemon, 'Focus Punch', 'Focus Punch');
 				return true;
 			}

data/scripts.js
@@ -315,7 +315,7 @@ exports.BattleScripts = {
 			this.damage(this.clampIntRange(Math.round(totalDamage * move.recoil[0] / move.recoil[1]), 1), pokemon, target, 'recoil');
 		}
 
-		if (target && move.category !== 'Status') target.gotAttacked(move, damage, pokemon);
+		if (target && pokemon !== target) target.gotAttacked(move, damage, pokemon);
 
 		if (!damage && damage !== 0) return damage;
 

commit 882d53ce1aae1fc1b91cf290d419a8125a126d30

Fix bug in Focus Punch stopped by hit on Sub
Fixes the bug introduced in 555bae44d10f508449e5e68f747c583dbad15a9d in which Focus Punch is stopped by hitting a Substitute, totally breaking the somewhat common SubPunch tactic.
It also now takes into account that damage should be higher than 0 if existing.

data/moves.js
@@ -4705,7 +4705,7 @@ exports.BattleMovedex = {
 			if (!pokemon.removeVolatile('focuspunch')) {
 				return false;
 			}
-			if (pokemon.lastAttackedBy && pokemon.lastAttackedBy.thisTurn && this.getMove(pokemon.lastAttackedBy.move).category !== 'Status') {
+			if (pokemon.lastAttackedBy && pokemon.lastAttackedBy.thisTurn && pokemon.lastAttackedBy.damage > 0 && this.getMove(pokemon.lastAttackedBy.move).category !== 'Status') {
 				this.add('cant', pokemon, 'Focus Punch', 'Focus Punch');
 				return true;
 			}