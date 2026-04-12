7501502b2c018d44185f82763176f85e632ced48
Ivo Julca <ivojulca@hotmail.com>
2026-04-11 22:03:01 -0500

    Refactor: Formats gameType, playerCount now stored/read to/from RuleTable

--- config/formats.ts
+++ config/formats.ts
@@ -1061,8 +1061,8 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 			'Technician', 'Tinted Lens', 'Triage', 'Unaware', 'Unburden', 'Water Bubble',
 		],
 		onValidateRule() {
-			if (this.format.gameType !== 'singles') {
-				throw new Error(`Shared Power currently does not support ${this.format.gameType} battles.`);
+			if (this.ruleTable.gameType !== 'singles') {
+				throw new Error(`Shared Power currently does not support ${this.ruleTable.gameType} battles.`);
 			}
 		},
 		getSharedPower(pokemon) {
@@ -2531,8 +2531,8 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 			'Choice Specs', 'Focus Band', 'Focus Sash', 'King\'s Rock', 'Quick Claw', 'Razor Fang', 'Baton Pass', 'Last Respects', 'Revival Blessing', 'Shed Tail',
 		],
 		onValidateRule() {
-			if (this.format.gameType !== 'singles') {
-				throw new Error(`Sharing is Caring currently does not support ${this.format.gameType} battles.`);
+			if (this.ruleTable.gameType !== 'singles') {
+				throw new Error(`Sharing is Caring currently does not support ${this.ruleTable.gameType} battles.`);
 			}
 		},
 		getSharedItems(pokemon) {
@@ -3757,8 +3757,8 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		bestOfDefault: true,
 		ruleset: ['[Gen 9] Random Battle', 'Team Preview', 'Max Team Size = 12', 'Picked Team Size = 6'],
 		onValidateRule() {
-			if (this.format.gameType !== 'singles') {
-				throw new Error(`Shared Power currently does not support ${this.format.gameType} battles.`);
+			if (this.ruleTable.gameType !== 'singles') {
+				throw new Error(`Shared Power currently does not support ${this.ruleTable.gameType} battles.`);
 			}
 		},
 		onBeforeSwitchIn(pokemon) {
@@ -3780,8 +3780,8 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 		bestOfDefault: true,
 		ruleset: ['[Gen 9] Random Battle', 'Team Preview', 'Camomons Mod', 'Inverse Mod', 'Scalemons Mod'],
 		onValidateRule() {
-			if (this.format.gameType !== 'singles') {
-				throw new Error(`Shared Power currently does not support ${this.format.gameType} battles.`);
+			if (this.ruleTable.gameType !== 'singles') {
+				throw new Error(`Shared Power currently does not support ${this.ruleTable.gameType} battles.`);
 			}
 		},
 		onBeforeSwitchIn(pokemon) {
